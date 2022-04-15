2dc9e71412d89adaa75b9cb1856212065e073e77
“AnkitaDroid7” <ankita.akatsuki@gmail.com>
2022-04-15 11:48:10 +0530

    intilaized frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter,Routes,Route,} from "react-router-dom";
+import AdminDashboard from './AdminDashboard';
 import './App.css';
+import RegisterUser from './RegisterUser';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<RegisterUser/>} />
+        <Route path='/admin' element={<AdminDashboard/>} />    
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
